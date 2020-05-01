f12d3f89d0ddf3d3416da51fd4da1a475bc76789
freddysilber <freddy.silber@cloudcoach.com>
2020-05-01 11:16:39 -0600

    migrating sfdx components

---
+++ react-lightning/force-app/main/default/applications/reactDevContainerApp/reactDevContainerApp.app
@@ -0,0 +1,3 @@
+<aura:application>
+    <c:ReactContainer />
+</aura:application>
---
+++ react-lightning/force-app/main/default/aura/ReactContainer/ReactContainerController.js
@@ -0,0 +1,20 @@
+({
+    handleMessage: function (component, event, helper) {
+        var message = event.getParams()
+        component.set('v.message', message.payload.value)
+    },
+
+    handleError: function (component, event, helper) {
+        var error = event.getParams()
+        console.log(error)
+    },
+
+    handleSendClick: function (component, event, helper) {
+        var messageText = component.get('v.message')
+        var message = {
+            name: "Send To React",
+            value: messageText
+        }
+        component.find('jsApp').message(message)
+    }
+})
