385e66b9d34728cbcfe1fe1db7ca77f886527bc6
freddysilber <freddy.silber@cloudcoach.com>
2020-05-01 12:30:23 -0600

    refactoring controller

--- force-app/main/default/aura/ReactContainer/ReactContainerController.js
+++ force-app/main/default/aura/ReactContainer/ReactContainerController.js
@@ -2,32 +2,26 @@
     doInit: function (component, event, helper) {
         const action = component.get('c.getAccounts')
         action.setCallback(this, response => {
-            const state = response.getState()
-            console.log(state)
-            if (state === 'SUCCESS') {
-                console.log(response.getReturnValue())
+            if (response.getState() === 'SUCCESS') {
                 component.set('v.accounts', response.getReturnValue())
-                console.log(component.get('v.accounts'))
-            } else {
-                console.log(response)
             }
         })
         $A.enqueueAction(action)
     },
 
     handleMessage: function (component, event, helper) {
-        var message = event.getParams()
+        const message = event.getParams()
         component.set('v.message', message.payload.value)
     },
 
     handleError: function (component, event, helper) {
-        var error = event.getParams()
-        console.log(error)
+        const error = event.getParams()
+        console.error(error)
     },
 
     handleSendClick: function (component, event, helper) {
-        var messageText = component.get('v.message')
-        var message = {
+        const messageText = component.get('v.message')
+        const message = {
             name: "Send To React",
             value: messageText
         }
